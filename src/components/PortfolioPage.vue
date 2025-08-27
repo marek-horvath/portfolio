e55649b746f1b15e113317cf998355c3a4b0c651
MarekH <marek.horvath@tuke.sk>
2025-08-27 10:06:25 +0200

    update

--- src/components/PortfolioPage.vue
+++ src/components/PortfolioPage.vue
@@ -74,107 +74,216 @@
 
       <!-- TABS CONTENT -->
       <div class="tab-content">
+
+
+
+
+
+
+
+
+
+
+
+
         <!-- PUBLICATIONS TAB -->
-        <div v-if="activeTab === 'Publications'" class="publications-tab">
-          <h2>Selected Publications</h2>
-          <div class="scrollable-content">
-            <div class="pub-block">
-              <h3>Puzzle-Driven Learning: Developing and Assessing IT Challenges</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Emília Pietriková<br/>
-                <strong>Conference:</strong> ICETA 2023<br/>
-                <strong>Link:</strong> 
-                <a href="https://www.iceta.sk/archive/2023/" target="_blank">
-                  www.iceta.sk/archive/2023
-                </a>
-              </p>
-            </div>
+<div v-if="activeTab === 'Publications'" class="publications-tab">
+  <h2>Selected Publications</h2>
+  <div class="scrollable-content">
+    
+    <!-- SAMI 2024 -->
+    <div class="pub-block">
+      <h3>An Experimental Comparison of Three Code Similarity Tools on Over 1,000 Student Projects</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Emília Pietriková<br/>
+        <strong>Conference:</strong> SAMI 2024<br/>
+        <strong>DOI:</strong> 
+        <a href="http://dx.doi.org/10.1109/sami60510.2024.10432863" target="_blank">
+          10.1109/sami60510.2024.10432863
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-1720-6
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>An Experimental Comparison of Three Code Similarity Tools on 1,000+ Projects</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Emília Pietriková<br/>
-                <strong>Conference:</strong> SAMI 2024<br/>
-                <strong>Link:</strong> 
-                <a href="https://conf.uni-obuda.hu/sami2024/general.html" target="_blank">
-                  conf.uni-obuda.hu/sami2024
-                </a>
-              </p>
-            </div>
+    <!-- SAMI 2025 -->
+    <div class="pub-block">
+      <h3>Exploring GPT-Generated Variations in C Programming Assignments</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Lenka Bubeňková, Emília Pietriková<br/>
+        <strong>Conference:</strong> SAMI 2025<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/sami63904.2025.10883288" target="_blank">
+          10.1109/sami63904.2025.10883288
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-7936-5
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>Adaptation of Automated Assessment System for Large Programming Courses</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Tomáš Kormaník, Jaroslav Porubän<br/>
-                <strong>Conference:</strong> ICPEC 2024<br/>
-                <strong>Link:</strong> 
-                <a href="https://www.icpeconf.org/" target="_blank">
-                  www.icpeconf.org
-                </a>
-              </p>
-            </div>
+    <div class="pub-block">
+      <h3>Code Reuse and Good Clones in Programming Education</h3>
+      <p>
+        <strong>Authors:</strong> Lenka Bubeňková, Emília Pietriková, Marek Horváth<br/>
+        <strong>Conference:</strong> SAMI 2025<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/sami63904.2025.10883291" target="_blank">
+          10.1109/sami63904.2025.10883291
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-7936-5
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>Using ChatGPT during Implementation of Programs in Education</h3>
-              <p>
-                <strong>Authors:</strong> Norbert Baláž, Jaroslav Porubän, Marek Horváth, Tomáš Kormaník<br/>
-                <strong>Conference:</strong> ICPEC 2024<br/>
-                <strong>Link:</strong> 
-                <a href="https://www.icpeconf.org/" target="_blank">
-                  www.icpeconf.org
-                </a>
-              </p>
-            </div>
+    <div class="pub-block">
+      <h3>Simplifying Education of Reactive Programming Concepts Using Comparative Application</h3>
+      <p>
+        <strong>Authors:</strong> Tomáš Kormaník, Marek Horváth, Lenka Bubeňková<br/>
+        <strong>Conference:</strong> SAMI 2025<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/SAMI63904.2025.10883129" target="_blank">
+          10.1109/SAMI63904.2025.10883129
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-7936-5
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>Stylistic Patterns in Source Code as Behavioral Biometric Markers</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth<br/>
-                <strong>Event:</strong> SCYR (PhD Workshop), Košice<br/>
-                <strong>Link:</strong> 
-                <a href="http://scyr.kpi.fei.tuke.sk/" target="_blank">
-                  scyr.kpi.fei.tuke.sk
-                </a>
-              </p>
-            </div>
+    <div class="pub-block">
+      <h3>Development of a MATLAB Application for Simulating the SEIQRD Pandemic Spread Model</h3>
+      <p>
+        <strong>Authors:</strong> Nikola Geciová, Lukáš Tomaščík, Marek Horváth<br/>
+        <strong>Conference:</strong> SAMI 2025<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/sami63904.2025.10883239" target="_blank">
+          10.1109/sami63904.2025.10883239
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-7936-5
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>CODE CLONES: A Novel Approach to Detecting Plagiarism in Binary Decomposition</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Filip Gurbál<br/>
-                <strong>Journal:</strong> Acta Electrotechnica et Informatica<br/>
-                <strong>Link:</strong> 
-                <a href="https://www.aei.tuke.sk/" target="_blank">
-                  www.aei.tuke.sk
-                </a>
-              </p>
-            </div>
+    <!-- ICPEC 2024 -->
+    <div class="pub-block">
+      <h3>Adaptation of Automated Assessment System for Large Programming Courses</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Tomáš Kormaník, Jaroslav Porubän<br/>
+        <strong>Conference:</strong> ICPEC 2024<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.4230/OASIcs.ICPEC.2024.4" target="_blank">
+          10.4230/OASIcs.ICPEC.2024.4
+        </a><br/>
+        <strong>ISBN:</strong> 978-3-95977-347-8
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>Improving Software Education with Technical Debt Analysis Tool</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Emília Pietriková, Jakub Šašak<br/>
-                <strong>Conference:</strong> ICETA 2024<br/>
-                <strong>Link:</strong> 
-                <a href="https://www.iceta.sk" target="_blank">
-                  www.iceta.sk
-                </a>
-              </p>
-            </div>
+    <div class="pub-block">
+      <h3>Using ChatGPT During Implementation of Programs in Education</h3>
+      <p>
+        <strong>Authors:</strong> Norbert Baláž, Jaroslav Porubän, Marek Horváth, Tomáš Kormaník<br/>
+        <strong>Conference:</strong> ICPEC 2024<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.4230/OASIcs.ICPEC.2024.18" target="_blank">
+          10.4230/OASIcs.ICPEC.2024.18
+        </a><br/>
+        <strong>ISBN:</strong> 978-3-95977-347-8
+      </p>
+    </div>
 
-            <div class="pub-block">
-              <h3>Scalability and Performance in Microservices and Monolithic Architectures</h3>
-              <p>
-                <strong>Authors:</strong> Marek Horváth, Vladyslav Sakhnenko, Filip Gurbál<br/>
-                <strong>Conference:</strong> Informatics<br/>
-                <strong>Link:</strong>
-                <a href="https://informatics.kpi.fei.tuke.sk/" target="_blank">
-                  informatics.kpi.fei.tuke.sk
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
+    <!-- Informatics 2024 -->
+    <div class="pub-block">
+      <h3>Comparison of Scalability and Performance in Microservices and Monolithic Architectures</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Vladyslav Sakhnenko, Filip Gurbáľ<br/>
+        <strong>Conference:</strong> Informatics 2024<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/Informatics62280.2024.10900892" target="_blank">
+          10.1109/Informatics62280.2024.10900892
+        </a>
+      </p>
+    </div>
+
+    <!-- ICETA 2023 -->
+    <div class="pub-block">
+      <h3>Puzzle-Driven Learning: Developing and Assessing IT Challenges</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Emília Pietriková<br/>
+        <strong>Conference:</strong> ICETA 2023<br/>
+        <strong>DOI:</strong> 
+        <a href="http://dx.doi.org/10.1109/iceta61311.2023.10344036" target="_blank">
+          10.1109/iceta61311.2023.10344036
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3503-7069-0
+      </p>
+    </div>
+
+    <!-- ICETA 2024 -->
+    <div class="pub-block">
+      <h3>Improving Software Education with Technical Debt Analysis Tool</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Emília Pietriková, Jakub Šašak<br/>
+        <strong>Conference:</strong> ICETA 2024<br/>
+        <strong>DOI:</strong> 
+        <a href="https://doi.org/10.1109/iceta63795.2024.10850816" target="_blank">
+          10.1109/iceta63795.2024.10850816
+        </a><br/>
+        <strong>ISBN:</strong> 979-8-3315-2770-9
+      </p>
+    </div>
+
+    <!-- Acta Electrotechnica -->
+    <div class="pub-block">
+      <h3>Code Clones: A Novel Approach to Detecting Plagiarism in Binary Decomposition of C Programs</h3>
+      <p>
+        <strong>Authors:</strong> Marek Horváth, Filip Gurbáľ<br/>
+        <strong>Journal:</strong> Acta Electrotechnica et Informatica<br/>
+        <strong>DOI:</strong> 
+        <a href="http://dx.doi.org/10.2478/aei-2024-0006" target="_blank">
+          10.2478/aei-2024-0006
+        </a><br/>
+        <strong>ISSN:</strong> 1335-8243
+      </p>
+    </div>
+
+    <!-- SCYR 2024, 2025 -->
+    <div class="pub-block">
+      <h3>Stylistic Patterns in Source Code as Behavioral Biometric Markers for Programmer Identification</h3>
+      <p>
+        <strong>Author:</strong> Marek Horváth<br/>
+        <strong>Conference:</strong> SCYR 2024<br/>
+        <strong>Link:</strong> 
+        <a href="https://scyr.fei.tuke.sk/index.php/proceedings/" target="_blank">
+          scyr.fei.tuke.sk
+        </a><br/>
+        <strong>ISBN:</strong> 978-80-553-3474-5
+      </p>
+    </div>
+
+    <div class="pub-block">
+      <h3>Programmer Identification Based on Source Code Stylometric Analysis and Behavioral Biometrics</h3>
+      <p>
+        <strong>Author:</strong> Marek Horváth<br/>
+        <strong>Conference:</strong> SCYR 2025<br/>
+        <strong>Link:</strong> 
+        <a href="https://scyr.fei.tuke.sk/index.php/proceedings/" target="_blank">
+          scyr.fei.tuke.sk
+        </a>
+      </p>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
         <!-- WORK TAB -->
         <div v-else-if="activeTab === 'Work'" class="work-tab">
